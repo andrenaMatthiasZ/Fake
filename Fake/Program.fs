b0a42bacd2a8793f427b2a7cd700a34445f2af70
Matthias Zeleny <mzeleny@andrena.de>
2018-09-30 13:57:14 +0200

    Close application when pressing escape

--- Fake/Program.fs
+++ Fake/Program.fs
@@ -1,8 +1,35 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Windows
+open System.Windows.Input
+open System.Diagnostics
+open ReadKey
+
+type KeyDirection = Down | Up | Left | Right
+  
+let nextStep = 
+    Console.WriteLine("next step")
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    
+    printfn "Press escape key to close."
+
+    let mutable escapeNotPressed = true
+    while escapeNotPressed do
+        if Console.KeyAvailable then
+            let keyInfo = Console.ReadKey(true)
+            let keyPressed = getKeyPressed keyInfo
+            match keyPressed with 
+                | KnownKey key-> 
+                    match key with 
+                    | Esc -> escapeNotPressed <- false
+                    | Arrow(_) -> nextStep
+                | Other -> nextStep
+                
+
+        else 
+            nextStep
+    
     0 // return an integer exit code
---
+++ Fake/ReadKey.fs
@@ -0,0 +1,30 @@
+﻿module ReadKey
+
+open System
+open System.Windows
+open System.Windows.Input
+open System.Diagnostics
+
+type KeyDirection = Down | Up | Left | Right
+
+type Direction =
+    | Some of KeyDirection
+    | None
+
+type Key = 
+    | Esc 
+    | Arrow of KeyDirection
+
+type KeyPressed =
+    | KnownKey of Key
+    | Other
+
+let getKeyPressed (keyInfo:ConsoleKeyInfo) =
+    let key = keyInfo.Key
+    match key with
+    | ConsoleKey.Escape -> KnownKey(Esc)
+    | ConsoleKey.DownArrow -> KnownKey(Arrow(Down))
+    | ConsoleKey.UpArrow -> KnownKey(Arrow(Up))
+    | ConsoleKey.LeftArrow -> KnownKey(Arrow(Left))
+    | ConsoleKey.RightArrow -> KnownKey(Arrow(Right))
+    | _ -> Other
